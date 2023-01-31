330cc77eae5c14beccb8b780f4ee01600faaa52c
Jara-XPS <araj_mont@yahoo.com>
2023-01-31 15:04:31 -0500

    new function for gif overlay

--- cam.py
+++ cam.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from datetime import datetime
+from PIL import Image
 
 camera = cv2.VideoCapture(0)
 
@@ -14,23 +15,25 @@ gifOverlay = False
 # In the future, we can use the timestamp as our file names
 img_counter = 0
 
-def gifOverlay(frame):
-    print("heehee")
+def doGifOverlay(frame, now):
     gif = cv2.VideoCapture('bad_news.gif')
 
+    frames = []
     while True:
         ret, gif_frame = gif.read()
         if not ret:
             break
 
-    gif_frame = gif_frame.astype('float32')
+        gif_frame = cv2.resize(gif_frame, (new_frame.shape[1], new_frame.shape[0]))
+        overlay = cv2.addWeighted(gif_frame, 0.5, new_frame, 0.5, 0)
+        cv2.putText(overlay, now, (20, 40), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2, cv2.LINE_AA)
+        frames.append(overlay)
 
-    result = cv2.addWeighted(frame, 0.7, gif_frame, 0.3, 0)
-
-    cv2.imshow("Gif Capture", result)
-    gif.release()
+    frames_pil = [Image.fromarray(frame) for frame in frames]
+    frames_pil[0].save("test.gif", save_all=True, append_images=frames_pil[1:], loop=0)
 
 def takePhoto(frame, name):
+    curr_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     if useGrayScale:
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         print(frame.shape)
@@ -51,6 +54,9 @@ def takePhoto(frame, name):
             frame = cv2.addWeighted(frame, 0.5, gray_img, 0.7, 0)
         else:
             frame = cv2.addWeighted(frame, 0.7, overlay_img, 0.4, 0)
+
+    if gifOverlay:
+        doGifOverlay(frame, curr_time)
 
     cv2.putText(frame, str(datetime.now()), (20, 40), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2, cv2.LINE_AA)
     cv2.imshow("{}".format(name), frame)
